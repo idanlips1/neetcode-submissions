6f413ce479513bc9be44fc40b4a154e324f2d232
Idan Lipschitz <154340057+idanlips1@users.noreply.github.com>
2026-09-23 20:09:12 +0300

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        HashSet<Character> charSet = new HashSet<>();
+        int res = 0;
+
+        for (char c : s.toCharArray()){
+            charSet.add(c);
+        }
+
+        for (char c : charSet){
+            int count = 0;
+            int l = 0;
+            for (int r = 0; r < s.length(); r++){
+                if (s.charAt(r) == c){
+                    count++;
+                }
+                while ((r - l + 1) - count > k){
+                    if (s.charAt(l) == c){
+                        count--;
+                    }
+                    l++;
+                }
+                res = Math.max((r - l + 1), res);
+            }
+            
+        }
+    return res;
+    }
+}
