caaa9b76b08988d652fe537f971d35822c529660
Idan Lipschitz <154340057+idanlips1@users.noreply.github.com>
2026-09-25 21:51:32 +0300

    Add: balanced-binary-tree - submission-8

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-8.java
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root)[0] == 1;
+    }
+
+    private int[] dfs(TreeNode root){
+        if (root == null){
+            return new int[]{1,0};
+        }
+
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+
+        boolean balanced = (left[0] == 1 && right[0] == 1 && Math.abs(left[1] - right[1]) <= 1);
+        int height = 1 + Math.max(left[1], right[1]);
+
+        return new int[]{balanced ? 1 : 0, height};
+    }
+
+
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ 
+
+class Solution {
+    private boolean isBalanced = true;
+    public boolean isBalanced(TreeNode root) {
+        height(root);
+        return isBalanced;
+    }
+
+    private int height(TreeNode root) {
+        if (root == null) return 0;
+        int left = height(root.left);
+        int right = height(root.right);
+        if (Math.abs(left - right) > 1) isBalanced = false;
+        return Math.max(left + 1, right + 1);
+    }
+}
+*/
+
